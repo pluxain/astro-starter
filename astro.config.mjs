cc5bd32ff06a3eecbd3c9b2c163d4523778d7c21
Pascal Luxain <pascal.luxain@gmail.com>
2024-12-10 07:42:56 +0100

    ref(tech): use astro:env for LOG_LEVEL
    Ref: #7

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,21 @@
 // @ts-check
-import { defineConfig } from "astro/config";
+import { defineConfig, envField } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import paraglide from "@inlang/paraglide-astro";
 
 // https://astro.build/config
 export default defineConfig({
+  env: {
+    schema: {
+      LOG_LEVEL: envField.enum({
+        context: "client",
+        access: "public",
+        default: "trace",
+        optional: true,
+        values: ["debug", "error", "fatal", "info", "trace", "warn"],
+      }),
+    },
+  },
   i18n: {
     defaultLocale: "en",
     locales: ["en"],
